9737445b03080d15a49022be9585dde965cf97a6
root <manz7ak@gmail.com>
2016-04-20 00:46:26 -0400

    added a while loop to make it run forever

--- disk-monitor.pl
+++ disk-monitor.pl
@@ -50,85 +50,89 @@ if ($smartctl_check eq ''){
 }
 
 
-# Get all the attached disks
-my $disks = `/bin/lsblk -l | grep disk | awk '{ print \$1 }'`;
-my @disks = split('\n', $disks);
-chomp for(@disks);
+while(1){ # To run as a cron instead, remove the while loop
+	# Get all the attached disks
+	my $disks = `/bin/lsblk -l | grep disk | awk '{ print \$1 }'`;
+	my @disks = split('\n', $disks);
+	chomp for(@disks);
 
-# Check the overall Disk Space usage
-my $disk_space_usage = `df -h | grep -v Filesystem |  awk '{ print \$1":"\$5":"\$6 }'`;
-my @disk_space_usage = split('\n', $disk_space_usage);
-for $line (@disk_space_usage){
+	# Check the overall Disk Space usage
+	my $disk_space_usage = `df -h | grep -v Filesystem |  awk '{ print \$1":"\$5":"\$6 }'`;
+	my @disk_space_usage = split('\n', $disk_space_usage);
+	for $line (@disk_space_usage){
 
-	my @parts = split(':', $line);
-	my $partition = $parts[0];
-	my $usage = $parts[1];
-	my $mount = $parts[2];
-	$usage =~ s/%//g;
-	if ($usage > $disk_usage_limit){
-		print "Sending email";
-		my $host = `hostname`;
-		system("echo \"Disk Usage Critical\n==============\nCurrent Usage: $usage\nMounted on: $mount\nPartition: $partition\n\" | mail -s \"Disk usage Alert from $host\" $email");
-		write_log("Disk usage Critical [$usage % used] : Mounted on [$mount]");
+		my @parts = split(':', $line);
+		my $partition = $parts[0];
+		my $usage = $parts[1];
+		my $mount = $parts[2];
+		$usage =~ s/%//g;
+		if ($usage > $disk_usage_limit){
+			print "Sending email";
+			my $host = `hostname`;
+			system("echo \"Disk Usage Critical\n==============\nCurrent Usage: $usage\nMounted on: $mount\nPartition: $partition\n\" | mail -s \"Disk usage Alert from $host\" $email");
+			write_log("Disk usage Critical [$usage % used] : Mounted on [$mount]");
 
-		# use it to keep track of the space and send an alert if it recovers
-		system("echo \"Storage:$usage:$partition\" >> disk-stat.log");
-	} else{
-		write_log("Disk usage OK [$usage % used] : Mounted on [$mount]");
-		# check if disk-stat.log contains any previous alert for the partition :$partition
-		$check = `grep -ic $partition disk-stat.log 2>/dev/null`;
-		chomp($check);
-		if ( $check > 0 ){
-			system(
-				"echo \"Disk space recovered on [$host] \nmount point : [$mount] \nPartition: $partition\" | mail -s \"[INFO] Disk space recovered on [$host]\" $email");
-			system("sed -i \"/$partition/d\" disk-stat.log");
+			# use it to keep track of the space and send an alert if it recovers
+			system("echo \"Storage:$usage:$partition\" >> disk-stat.log");
+		} else{
+			write_log("Disk usage OK [$usage % used] : Mounted on [$mount]");
+			# check if disk-stat.log contains any previous alert for the partition :$partition
+			$check = `grep -ic $partition disk-stat.log 2>/dev/null`;
+			chomp($check);
+			if ( $check > 0 ){
+				system(
+					"echo \"Disk space recovered on [$host] \nmount point : [$mount] \nPartition: $partition\" | mail -s \"[INFO] Disk space recovered on [$host]\" $email");
+				system("sed -i \"/$partition/d\" disk-stat.log");
+			}
 		}
 	}
-}
 
-# Loop through the disks and check the load on each
-# also check the health.
-for $disk (@disks){
-	write_log("------------------------------------------");
-	write_log("Checking health and load of disk : [$disk]");
-	write_log("------------------------------------------");
-	my $disk_io_usage = `/usr/bin/iostat -dhx /dev/$disk | awk '\$13 ~ /^[0-9,\.]+\$/ { print \$13 }'`;
-	chomp ($disk_io_usage);
-	
-	# Disk health
-	my $disk_health = `smartctl -H /dev/$disk | grep overall-health | awk -F: '{ print \$2 }'`;
+	# Loop through the disks and check the load on each
+	# also check the health.
+	for $disk (@disks){
+		write_log("------------------------------------------");
+		write_log("Checking health and load of disk : [$disk]");
+		write_log("------------------------------------------");
+		my $disk_io_usage = `/usr/bin/iostat -dhx /dev/$disk | awk '\$13 ~ /^[0-9,\.]+\$/ { print \$13 }'`;
+		chomp ($disk_io_usage);
+		
+		# Disk health
+		my $disk_health = `smartctl -H /dev/$disk | grep overall-health | awk -F: '{ print \$2 }'`;
 
-	my $notify_flag;
-	if ($disk_io_usage > $disk_io_limit){
-		write_log("[WARN] Disk IO Critical. % Utilization :[$disk_io_usage]");
-		$notify_flag = 1;
-	} else {
-		write_log("[OK] Disk IO Utilization OK [$disk_io_usage]%");
-	}
-
-	$disk_health =~ s/^\s+//;
-	if ( $disk_health =~ /PASSED/){
-		write_log("[OK] Disk health OK");
-	} else { 											# Mayday mayday :D !!!
-		write_log("[FATAL] Disk Health Not OK");
-		$notify_flag = 1;
-		$emergency_flag = 1;
-	}
-
-	if ($notify_flag){
-		$host = `hostname`;
-		write_log("Sending Alerts to the Team");
-		if($emergency_flag){ # If the disk is in bad condition, there is no point in sending an alert for high IO usage
-			system(
-				"echo \"Disk Failure. Disk [$disk] on [$hostname] failed health test\" | 
-				 mail -s \"[FATAL] Disk [$disk] Health Fatal on host [$hostname] \" $email"
-				);
-		} else {  
-			system(
-				"echo \"Disk IO usage Critical on host [$hostname]\n--------------\n%Utilization: $disk_io_usage\n\" | 
-				 mail -s \"[WARN] Disk IO usage critical on [$host]\" $email"
-				 );
+		my $notify_flag;
+		if ($disk_io_usage > $disk_io_limit){
+			write_log("[WARN] Disk IO Critical. % Utilization :[$disk_io_usage]");
+			$notify_flag = 1;
+		} else {
+			write_log("[OK] Disk IO Utilization OK [$disk_io_usage]%");
 		}
+
+		$disk_health =~ s/^\s+//;
+		if ( $disk_health =~ /PASSED/){
+			write_log("[OK] Disk health OK");
+		} else { 											# Mayday mayday :D !!!
+			write_log("[FATAL] Disk Health Not OK");
+			$notify_flag = 1;
+			$emergency_flag = 1;
+		}
+
+		if ($notify_flag){
+			$host = `hostname`;
+			write_log("Sending Alerts to the Team");
+			if($emergency_flag){ # If the disk is in bad condition, there is no point in sending an alert for high IO usage
+				system(
+					"echo \"Disk Failure. Disk [$disk] on [$hostname] failed health test\" | 
+					 mail -s \"[FATAL] Disk [$disk] Health Fatal on host [$hostname] \" $email"
+					);
+			} else {  
+				system(
+					"echo \"Disk IO usage Critical on host [$hostname]\n--------------\n%Utilization: $disk_io_usage\n\" | 
+					 mail -s \"[WARN] Disk IO usage critical on [$host]\" $email"
+					 );
+			}
+		}
+		write_log("------------------------------------------");
 	}
-	write_log("------------------------------------------");
+
+sleep(60);
 }
